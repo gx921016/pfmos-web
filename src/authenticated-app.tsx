56bc9030f483b68e034e0d795b66109140280b2e
Shawn Gao <gx921016@gmail.com>
2022-06-20 10:25:28 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -3,17 +3,26 @@ import { useAuth } from "./context/auth-context";
 import ProjectListScreen from "./screens/project-list";
 import styled from "@emotion/styled";
 import { Button } from "antd";
+import { Row } from "./components/lib";
 
 const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <header>
-        <Button onClick={logout}>登出</Button>
-      </header>
-      <main>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button onClick={logout}>登出</Button>
+        </HeaderRight>
+      </Header>
+
+      <Main>
         <ProjectListScreen />
-      </main>
+      </Main>
     </Container>
   );
 };
@@ -26,9 +35,16 @@ const AuthenticatedApp = () => {
 //   height: calc(100vh - 6rem);
 // `;
 
+// temporal dead zone(暂时性死区)
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem calc(100vh - 6rem);
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
 `;
 
+// grid-area 用来给grid子元素起名字
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+const Main = styled.main``;
 export default AuthenticatedApp;
